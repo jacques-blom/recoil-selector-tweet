df9a8e4561a247d299e50652714bff70d3d7891e
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-10-12 14:30:48 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {selector, useRecoilValue} from 'recoil'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const passwordLengthSelector = selector({
+    key: 'passwordLength',
+    get: () => {
+        return 10
+    },
+})
+
+const PasswordLength = () => {
+    const passwordLength = useRecoilValue(passwordLengthSelector)
+
+    return (
+        <div>
+            <div>Password length:</div>
+            <div>{passwordLength}</div>
+        </div>
+    )
 }
 
-export default App;
+function App() {
+    return (
+        <div className="App">
+            <PasswordLength />
+        </div>
+    )
+}
+
+export default App
