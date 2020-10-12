ca70ea1584b038bacc1d21b78d1ea232f72a3fbf
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-10-12 14:40:03 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
-import {atom, selector, useRecoilValue} from 'recoil'
+import {atom, selector, useRecoilState, useRecoilValue} from 'recoil'
+
+const passwordState = atom({
+    key: 'password',
+    default: '',
+})
 
 const passwordLengthState = selector({
     key: 'passwordLength',
-    get: () => {
-        return 10
+    get: ({get}) => {
+        // ✨ Use the passwordState value
+        const password = get(passwordState)
+        return password.length
     },
 })
 
 const PasswordLength = () => {
     const passwordLength = useRecoilValue(passwordLengthState)
 
-    return (
-        <div>
-            <div>Password length:</div>
-            <div>{passwordLength}</div>
-        </div>
-    )
+    return <div>Password length: {passwordLength}</div>
+}
+
+const PasswordInput = () => {
+    const [password, setPassword] = useRecoilState(passwordState)
+
+    return <input value={password} onChange={(e) => setPassword(e.target.value)} />
 }
 
 function App() {
     return (
         <div className="App">
+            <PasswordInput />
             <PasswordLength />
         </div>
     )
